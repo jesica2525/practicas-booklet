eb8989a411698136e006ff59ba972f49689dfd2d
Jesica Rossi <jesipr@hotmail.com>
2022-06-23 10:25:20 -0300

    Modificacion Ejercicio 1.4

--- Ejercicio1.4/app.js
+++ Ejercicio1.4/app.js
@@ -1,14 +1,14 @@
-integrante1 = prompt ("Ingrese el nombre del primer integrante");
-integrante2 = prompt ("Ingrese el nombre del segundo integrante");
-integrante3 = prompt ("Ingrese el nombre del tercer integrante");
-integrante4 = prompt ("Ingrese el nombre del cuarto integrante");
-integrante5 = prompt ("Ingrese el nombre del quinto integrante");
-integrante1 = prompt ("Ingrese el nombre del primer integrante");
+integrante1 = prompt ("Ingrese el nombre del primer integrante de la familia");
+integrante2 = prompt ("Ingrese el nombre del segundo integrante de la familia");
+integrante3 = prompt ("Ingrese el nombre del tercer integrante de la familia");
+integrante4 = prompt ("Ingrese el nombre del cuarto integrante de la familia");
+integrante5 = prompt ("Ingrese el nombre del quinto integrante de la familia");
 
-let integrantes = ("1) " +integrante1+" "+
-                  "2) " +integrante2+" "+
-                  "3) " +integrante3+" "+
-                  "4) " +integrante4+" "+
-                  "5) " +integrante1);
+let integrantes = ("Los nombres de los integrantes de la familia son: \n" +
+                  "1) " + integrante1 +"\n "+
+                  "2) " +integrante2 +"\n "+
+                  "3) " +integrante3+"\n "+
+                  "4) " +integrante4+"\n "+
+                  "5) " +integrante5);
 
 alert(integrantes);
